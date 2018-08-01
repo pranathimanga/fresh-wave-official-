81973ca87521d37ee699dc6cf44a60e003b72632
pranathimanga <41089418+pranathimanga@users.noreply.github.com>
2018-08-01 13:03:09 -0700

    Add files via upload

---
+++ parallax.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+	$('#nav').localScroll(800);
+
+
+	//.parallax(xPosition, speedFactor, outerHeight) options:
+
+	//xPosition - Horizontal position of the element
+
+	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+
+	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+
+	$('#intro').parallax("50%", 0.1);
+
+	$('#second').parallax("50%", 0.1);
+
+	// $('.bg').parallax("50%", 0.4);
+
+	$('#third').parallax("50%", 0.3);
+
+  $('#fourth').parallax("50%", 0.5);
+
+  $('#fifth').parallax("50%", 0.1);
+
+  $('#sixth').parallax("50%", 0.1);
+
+  $('#seven').parallax("50%", 0.2);
+
+})
---
+++ products.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('#nav').localScroll(800);
+
+	//.parallax(xPosition, speedFactor, outerHeight) options:
+	//xPosition - Horizontal position of the element
+	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+	$('#intro').parallax("50%", 0.1);
+	$('#second').parallax("50%", 0.1);
+
+})
